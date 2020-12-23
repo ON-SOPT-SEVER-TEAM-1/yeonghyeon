83bb379fd6c318d5032ceb7838a6f24362852d3b
yeonghyeonSeok <syh4834@naver.com>
2020-12-23 14:00:30 +0900

    seminar_05

---
+++ 5th-seminar/routes/posts/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../../controller/postController');
+const multer = require('multer');
+const upload = multer({
+  dest: 'upload/'
+});
+
+router.post('/', postController.createPost);
+router.get('/', postController.readAllPosts);
+router.post('/:postId/like', postController.createLike);
+router.delete('/:postId/like', postController.deleteLike);
+
+module.exports = router;
